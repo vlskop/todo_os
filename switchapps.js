c7bc876868c62e0c439e4c45f33616d31adc899d
Vladimir Skop <v.skop@mail.ru>
2022-07-06 16:07:31 +0300

    stable version (calculator working)

--- switchapps.js
+++ switchapps.js
@@ -1,37 +1,37 @@
 let menu = document.querySelector('.workflow-apps'); // меню
 let menuBtns = menu.children; // элементы меню
 
-let workfield = document.querySelector('.workfield');
-let workfieldElements = workfield.children;
+let workfield = document.querySelector('.workfield'); // рабочее пространство
+let workfieldElements = workfield.children; // элементы рабочего пространства
 
-let activeBtn = document.querySelector('.menu-active');
+let activeBtn = document.querySelector('.menu-active'); // активная кнопка меню
 
-let activeParent = activeBtn.parentNode;
+let activeParent = activeBtn.parentNode; // родитель активной кнопки
 
-
-let indexChecker = function() {
+// поиск индекса активной кнопки и переход к другому приложению
+let indexChecker = function () {
     for (let k = 0; k < menuBtns.length; k++) {
-            if (activeParent.children[k].classList.contains('menu-active')) {
-                let index = k;
-                setTimeout(() => {
-                    workfieldElements[index].classList.add('app-switch-backward');
-                    workfieldElements[index].classList.remove('app-switch-forward');   
-                    workfieldElements[index].classList.remove('hidden');                     
-                }, 500);
- 
-             }
-             else {
-                workfieldElements[k].classList.add('app-switch-forward');
-                    workfieldElements[k].classList.remove('app-switch-backward');
-                    setTimeout(() => {
-                    workfieldElements[k].classList.add('hidden');       
-                    }, 500);          
-             }
+        if (activeParent.children[k].classList.contains('menu-active')) {
+            let index = k;
+            setTimeout(() => {
+                workfieldElements[index].classList.add('app-switch-backward');
+                workfieldElements[index].classList.remove('app-switch-forward');
+                workfieldElements[index].classList.remove('hidden');
+            }, 600);
+
+        } else {
+            workfieldElements[k].classList.add('app-switch-forward');
+            workfieldElements[k].classList.remove('app-switch-backward');
+            setTimeout(() => {
+                workfieldElements[k].classList.add('hidden');
+            }, 600);
+        }
     }
 };
 
-let pressChecker = function(button) { // обработчик клика по кнопкам меню
-    button.onclick = function() {
+// обработчик клика по кнопкам меню
+let pressChecker = function (button) {
+    button.onclick = function () {
         for (let menuBtn of menuBtns) {
             menuBtn.classList.remove('menu-active');
         }
@@ -46,11 +46,10 @@ for (let i = 0; i < menuBtns.length; i++) {
     pressChecker(menuBtns[i]);
 }
 
-window.addEventListener('load', function(){
+// добавление обработчика при загрузке страницы
+window.addEventListener('load', function () {
     indexChecker();
     for (let i = 0; i < menuBtns.length; i++) {
         pressChecker(menuBtns[i]);
     }
 });
-
-localStorage.clear();
